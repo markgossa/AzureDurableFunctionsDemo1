7a5caa724a9252dffadfbb1f973b9bb9f19e39fd
Mark Gossa <mark@gossa.co.uk>
2019-01-15 15:31:23 +0000

    Update function app version to v2

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -31,4 +31,5 @@ resource "azurerm_function_app" "functionApp1" {
   resource_group_name       = "${azurerm_resource_group.resourceGroup1.name}"
   app_service_plan_id       = "${azurerm_app_service_plan.appServicePlan1.id}"
   storage_connection_string = "${azurerm_storage_account.storageAccount1.primary_connection_string}"
+  version                   = "beta"
 }
